5687e8ab0a0e57984ebb169a201267a3691e3517
lixiang <3192755534@qq.com>
2023-11-14 10:43:09 +0800

    🐶feat: react-router redirect study

--- src/Router/router/index.js
+++ src/Router/router/index.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react'
-import { Route, HashRouter } from 'react-router-dom'
+import { Route, HashRouter, Redirect, Switch } from 'react-router-dom'
 import Film from '../views/film'
 import Cinema from '../views/cinema'
 import Center from '../views/center'
-
+import NotFound from '../views/NotFound'
 
 export default class Index extends Component {
   render() {
     return (
       <HashRouter>
-        <Route path="/film" component={Film}></Route>
-        <Route path="/cinema" component={Cinema}></Route>
-        <Route path="/center" component={Center}></Route>
+        <Switch>
+          <Route path="/film" component={Film}></Route>
+          <Route path="/cinema" component={Cinema}></Route>
+          <Route path="/center" component={Center}></Route>
+          {/* 模糊匹配 */}
+          <Redirect from="/" to="/film" exact></Redirect>
+          {/* 精确匹配 exact */}
+          <Route component={NotFound}></Route>
+        </Switch>
       </HashRouter>
     )
   }
--- src/Router/views/film.js
+++ src/Router/views/film.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import NowPlaying from './film/NowPlaying'
+import Comingsoon from './film/Comingsoon'
 
 export default function Film() {
   return (
-    <div>film</div>
+    <div>
+      <div style={{
+        height: '200px',
+        backgroundColor: 'yellow'
+      }}>
+        大轮播
+      </div>
+      <div>导航栏</div>
+      <Switch>
+        <Route path="/film/nowplaying" component={ NowPlaying }></Route>
+        <Route path="/film/comingsoon" component={ Comingsoon }></Route>
+        <Redirect from="/film" to="/film/nowplaying"></Redirect>
+      </Switch>
+    </div>
   )
 }
