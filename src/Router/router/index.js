23f42ad7f42d1e1a02ed0d6c29304cb5492b453f
lixiang <3192755534@qq.com>
2023-11-29 11:12:47 +0800

    🐶feat: router redirect

--- src/Router/router/index.js
+++ src/Router/router/index.js
@@ -4,7 +4,12 @@ import Film from '../views/film'
 import Cinema from '../views/cinema'
 import Center from '../views/center'
 import Detail from '../views/detail'
+import Login from '../views/login'
 import NotFound from '../views/NotFound'
+
+function isAuth () {
+  return localStorage.getItem('token')
+}
 
 export default class Index extends Component {
   render() {
@@ -14,7 +19,11 @@ export default class Index extends Component {
         <Switch>
           <Route path="/film" component={Film}></Route>
           <Route path="/cinema" component={Cinema}></Route>
-          <Route path="/center" component={Center}></Route>
+          {/* <Route path="/center" component={Center}></Route> */}
+          <Route path="/center" render={() => {
+            return isAuth() ? <Center /> : <Redirect to="/login" />
+          }}></Route>
+          <Route path="/login" component={Login}></Route>
           {/* /detail/1111  动态路由 */}
           {/* <Route path="/detail/:id" component={Detail}></Route> */}
           <Route path="/detail" component={Detail}></Route>
